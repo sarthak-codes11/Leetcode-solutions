2618849e07eee12bfd6e5d3deca589a6e6daba4b
Sarthak Dilip Deore <sarthak.dilip.deore@gmail.com>
2026-09-06 15:44:02 +0530

    Time: 3 ms (86.05%) | Memory: 43.3 MB (40.86%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for(char ch : s.toCharArray()){
-            if(ch == '(' || ch == '{' || ch == '['){
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
                 stack.push(ch);
-            }else{
-                if(stack.isEmpty()){
+            } else {
+                if (stack.isEmpty()) {
                     return false;
                 }
                 char top = stack.pop();
-                if(ch == ')' && top != '('){
+                if (ch == ')' && top != '(') {
                     return false;
                 }
-                if(ch == '}' && top != '{')
+                if (ch == ']' && top != '[') {
                     return false;
-
-                if(ch == ']' && top != '[')
+                }
+                if (ch == '}' && top != '{') {
                     return false;
+                }
             }
         }
-         return stack.isEmpty();
+        return stack.isEmpty();
     }
-   
 }
